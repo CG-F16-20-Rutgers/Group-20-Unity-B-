22acf673d071abb2f5d57329aa915d988767c703
Tgood5 <kilpert3@gmail.com>
2016-11-22 08:08:56 -0500

    B4

---
+++ Assets/B4BehaviorTree.cs
@@ -0,0 +1,295 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using TreeSharpPlus;
+
+public class B4BehaviorTree : MonoBehaviour
+{
+	
+	public Transform[] hidingSpots;
+	public Transform playerPosition;
+	public Transform countspot;
+	public Transform meetspot;
+	public Transform winspot;
+	public GameObject friend1;
+	public GameObject friend2;
+	public GameObject friend3;
+	public GameObject friend4;
+	public GameObject playagent;
+
+	public Text scoretext;
+	public Text wintext;
+	public Text timer;
+
+	public int score;
+	public float timeleft;
+
+	public bool f1found;
+	public bool f2found;
+	public bool f3found;
+	public bool f4found;
+	public bool timeup;
+
+	private BehaviorAgent behaviorAgent;
+	// Use this for initialization
+	void Start ()
+	{
+		wintext.text = "";
+		f1found = false;
+		f2found = false;
+		f3found = false;
+		f4found = false;
+		timeup = false;
+		score = 0;
+		this.updateScore();
+		behaviorAgent = new BehaviorAgent (this.BuildTreeRoot ());
+		BehaviorManager.Instance.Register (behaviorAgent);
+		behaviorAgent.StartBehavior ();
+		timeleft = 300.0f;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				//if (hit.transform.tag == "friend") {
+				//	f1found = true;
+				//	score++;
+				//	updateScore ();
+				//}
+				playerPosition.position = (Val.V (() =>  hit.point)).Value;
+				UpdatePlayerPosition();
+			}
+		}
+		timeleft -= Time.deltaTime;
+		int timei = (int)Math.Round(timeleft);
+		if (timei <= 0) {
+			timei = 0;
+			timeup = true;
+			wintext.text = "Time's up!";
+		}
+		timer.text = "time remaining: " + timei;
+	}
+
+	void updateScore(){
+		score = 0;
+		if (f1found)
+			score++;
+		if (f2found)
+			score++;
+		if (f3found)
+			score++;
+		if (f4found)
+			score++;
+		scoretext.text = "Friends found: " + score;
+		if (score == 4)
+			scoretext.text = "You found everyone!";
+	}
+
+	protected Node ST_ApproachAndWait(GameObject participant, Transform target)
+	{
+		Val<Vector3> position = Val.V (() => target.position);
+		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+	}
+
+	//get close to and face target point
+	protected Node ST_ApproachAndOrient(GameObject participant, Transform target)
+	{
+		Val<Vector3> position = Val.V (() => target.position);
+		return new Sequence( 
+			participant.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(position, 2.0f), 
+			new LeafWait(1000),
+			participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(position),
+			new LeafWait(1000)
+		);
+	}
+
+
+	protected Node BuildTreeRoot()
+	{
+
+		//part one ai controlled
+		Node searcher = new DecoratorLoop (
+			new Sequence(
+				new SequenceShuffle(
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+					this.ST_ApproachAndWait(playagent, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)])
+				))  );
+
+		//player control part2
+		Node clicker = new DecoratorLoop (
+			new Sequence(this.ST_ApproachAndWait(playagent, playerPosition)));
+
+		//start nodes
+		Node startcount = new Sequence (
+			this.ST_ApproachAndWait(this.playagent, playerPosition),
+			playagent.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("WAVE", 2000),
+			this.ST_ApproachAndWait(this.playagent, countspot), 
+			new LeafWait(9000),
+			this.ST_ApproachAndWait(this.playagent, playerPosition));
+
+		//ROOT NODE-------------------------------------------------------------------------------------------------------------
+		Node root = new DecoratorLoop (
+			
+			new SequenceParallel(
+				//use searcher for part1, clicker for part2
+				new Sequence(startcount, searcher), 
+				this.friendNode(friend1),
+				this.friendNode(friend2),
+				this.friendNode(friend3),
+				this.friendNode(friend4),
+				this.AssertFoundAll()
+			)
+			
+		);
+
+		return root;
+	}
+
+	protected Node friendNode(GameObject friend){
+		return new Sequence (
+			new LeafWait(8000),
+			this.ST_ApproachAndWait(friend, hidingSpots[UnityEngine.Random.Range(0,hidingSpots.Length-1)]),
+			this.AssertFriendFound(friend),
+			new DecoratorLoop(friend.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("CLAP", 3000))
+		);
+	}
+
+	protected Node ST_GreetActors(GameObject actor1, GameObject actor2)
+	{
+		return new Sequence (
+			this.ST_ApproachAndOrient(actor2, actor1.transform),
+			actor2.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("WAVE", 2000)
+		);
+	}
+
+	protected Node ST_ConverseActors(GameObject actor1, GameObject actor2)
+	{
+		return new DecoratorLoop(
+			new Sequence (
+				actor1.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("THINK", 3000),
+				actor2.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("CLAP", 3000)
+			)
+		);
+	}
+
+	bool DetectActors(GameObject detector)
+	{
+		Collider[] Colliders;
+		Colliders = Physics.OverlapSphere(detector.transform.position, 3);
+		for(int i =0; i<Colliders.Length; i++){
+			if(Colliders[i].gameObject.tag == "Player"){
+				if (detector.Equals(this.friend1)) {
+					f1found = true;
+				}
+				if (detector.Equals(this.friend2)) {
+					f2found = true;
+				}
+				if (detector.Equals(this.friend3)) {
+					f3found = true;
+				}
+				if (detector.Equals(this.friend4)) {
+					f4found = true;
+				}
+				updateScore ();
+				return true;
+			}
+		}
+		return false;
+	}
+		
+
+
+	//mouse controls
+	void PointOnMap()
+	{
+		Val<Ray> ray = Val.V (() => Camera.main.ScreenPointToRay (Input.mousePosition));
+		RaycastHit hit;
+		Physics.Raycast (ray.Value, out hit, 100);
+		Val.V (() => Physics.Raycast (ray.Value, out hit, 100));
+		playerPosition.position = (Val.V (() =>  hit.point)).Value;
+		UpdatePlayerPosition();
+	}
+
+	void UpdatePlayerPosition()
+	{
+		Val<Vector3> position = playerPosition.position;
+		playagent.GetComponent<SteeringController> ().Target = position.Value;
+	}
+
+	protected Node AssertFriendFound(GameObject friend)
+	{
+		return new DecoratorLoop(new Sequence(new DecoratorInvert(new DecoratorLoop (new DecoratorInvert(new Sequence(this.CheckFound (friend))))),
+			this.friend1Found(friend)
+			//new LeafWait(1000)
+		));
+	}
+
+	protected Node CheckFound(GameObject friend)
+	{
+		return new LeafAssert (() => DetectActors(friend));
+	}
+
+	protected Node friend1Found(GameObject friend){
+		//wintext.text="you found "+friend.name;
+		return new Sequence(
+			playagent.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("WAVE", 2000),
+			this.ST_ApproachAndOrient(friend, playagent.transform),
+			friend.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("WAVE", 2000),
+			this.ST_ApproachAndWait(friend, meetspot),
+			friend.GetComponent<BehaviorMecanim>().Node_OrientTowards(winspot.position)
+		);
+	}
+
+	protected Node AssertFoundAll()
+	{
+		return new DecoratorLoop(new Sequence(new DecoratorInvert(new DecoratorLoop (new DecoratorInvert(new Sequence(this.CheckFoundAll ())))),
+			new LeafWait(5000),
+			this.endgame()
+		));
+	}
+
+	protected Node CheckFoundAll()
+	{
+		return new LeafAssert (() => score==4);
+	}
+
+	protected Node AssertTimeCheck()
+	{
+		return new DecoratorLoop(new Sequence(new DecoratorInvert(new DecoratorLoop (new DecoratorInvert(new Sequence(this.CheckTime ())))),
+			new LeafWait(5000),
+			this.endgame()
+
+		));
+	}
+
+	protected Node CheckTime()
+	{
+		return new LeafAssert (() => timeleft<=0.1f);
+	}
+
+	protected Node endgame(){
+		if(score==4)
+			playerPosition = winspot;
+		return new Sequence (
+			this.ST_ApproachAndWait (playagent, winspot),
+			playagent.GetComponent<BehaviorMecanim>().Node_OrientTowards(meetspot.position),
+		
+			playagent.GetComponent<BehaviorMecanim> ().ST_PlayHandGesture ("SURPRISED", 3000)
+			
+		);
+	}
+}
+
---
+++ Assets/MainBehaviorTree.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using TreeSharpPlus;
+
+public class MainBehaviorTree : MonoBehaviour
+{
+	public Transform wander1;
+	public Transform wander2;
+	public Transform wander3;
+	public Transform patrol1;
+	public Transform patrol2;
+	public Transform patrol3;
+	public Transform patrol4;
+	public Transform positionToMove;
+	public GameObject participant;
+	public GameObject actor;
+	public GameObject police;
+	public Transform mousepoint;
+
+	private BehaviorAgent behaviorAgent;
+	// Use this for initialization
+	void Start ()
+	{
+		behaviorAgent = new BehaviorAgent (this.BuildTreeRoot ());
+		BehaviorManager.Instance.Register (behaviorAgent);
+		behaviorAgent.StartBehavior ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				positionToMove.position = (Val.V (() =>  hit.point)).Value;
+				UpdatePlayerPosition();
+			}
+		}
+	}
+
+	protected Node ST_ApproachAndWait(GameObject participant, Transform target)
+	{
+		Val<Vector3> position = Val.V (() => target.position);
+		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+	}
+
+	//get close to and face target point
+	protected Node ST_ApproachAndOrient(GameObject participant, Transform target)
+	{
+		Val<Vector3> position = Val.V (() => target.position);
+		return new Sequence( 
+			participant.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(position, 2.0f), 
+			new LeafWait(1000),
+			participant.GetComponent<BehaviorMecanim>().Node_OrientTowards(position),
+			new LeafWait(1000)
+			);
+	}
+		
+
+	protected Node BuildTreeRoot()
+	{
+		Val<float> pp = Val.V (() => police.transform.position.z);
+		Val<Vector3> position1 = Val.V (() => participant.transform.position);
+		Val<Vector3> position2 = Val.V (() => actor.transform.position);
+		Func<bool> detect = () => (police.transform.position.z > 9.9f);
+		Func<bool> act = () => (police.transform.position.z < 10.1f);
+		//actors roam at random
+		Node roaming1 = new DecoratorLoop (
+			new Sequence(
+				new SequenceShuffle(
+					this.ST_ApproachAndWait(this.participant, this.wander1),
+					this.ST_ApproachAndWait(this.participant, this.wander2),
+					this.ST_ApproachAndWait(this.participant, this.wander3))));
+		Node roaming2 = new DecoratorLoop(
+			new Sequence(
+				new SequenceShuffle(
+					this.ST_ApproachAndWait(this.actor, this.wander3),
+					this.ST_ApproachAndWait(this.actor, this.wander2),
+					this.ST_ApproachAndWait(this.actor, this.wander1))));
+		Node patrol1 = new DecoratorLoop(
+			new Sequence(
+				this.ST_ApproachAndWait(this.police, this.patrol4),
+				this.ST_ApproachAndWait(this.police, this.patrol3),
+				this.ST_ApproachAndWait(this.police, this.patrol2)));
+		Node patrol2 = new DecoratorLoop(
+			new Sequence(
+				this.ST_ApproachAndWait(this.police, this.patrol2),
+				this.ST_ApproachAndWait(this.police, this.patrol1),
+				this.ST_ApproachAndWait(this.police, this.patrol4)));
+		Node trigger = new DecoratorLoop (new LeafAssert (act));
+		Node meetTrigger = new DecoratorLoop (new LeafAssert (detect));
+		Node hide1 = new Sequence(
+				new SequenceShuffle(
+					this.ST_ApproachAndWait(this.participant, this.wander1),
+					this.ST_ApproachAndWait(this.participant, this.wander2),
+					this.ST_ApproachAndWait(this.participant, this.wander3)));
+		Node meetup = new DecoratorLoop (
+						new Sequence (
+							ST_GreetActors(participant, actor),
+							new LeafWait(1000),
+							ST_ConverseActors(participant, actor))
+					);
+
+		Node clicker = new DecoratorLoop (
+			new Sequence(this.ST_ApproachAndWait(this.police, this.positionToMove)));
+
+		//ROOT NODE
+		Node root = new DecoratorLoop (new SequenceParallel(
+			
+			new DecoratorForceStatus (RunStatus.Success, 
+				new SequenceParallel(roaming1, roaming2, clicker))
+			
+			)
+			);
+		return root;
+	}
+
+	protected Node ST_GreetActors(GameObject actor1, GameObject actor2)
+	{
+		return new SequenceParallel (
+			ST_ApproachAndOrient(actor1, actor2.transform),
+			ST_ApproachAndOrient(actor2, actor1.transform),
+			actor1.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("WAVE", 2000),
+			actor2.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("WAVE", 2000)
+			);
+	}
+
+	protected Node ST_ConverseActors(GameObject actor1, GameObject actor2)
+	{
+		return new DecoratorLoop(
+			new Sequence (
+			actor1.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("THINK", 3000),
+			actor2.GetComponent<BehaviorMecanim>().ST_PlayHandGesture("CLAP", 3000)
+			)
+		);
+	}
+
+	protected Node ST_DetectActors(GameObject actor1, GameObject actor2)
+	{
+		Val<Vector3> position1 = Val.V (() => actor1.transform.position);
+		Val<Vector3> position2 = Val.V (() => actor2.transform.position);
+		float detectdist = 5.0f;
+		float xc = (position1.Value - position2.Value).magnitude;
+		if ( xc <= detectdist) {
+			return new Sequence (
+				ST_GreetActors (participant, actor),
+				new LeafWait (1000),
+				ST_ConverseActors (participant, actor));
+		}else{
+			return new LeafWait(1000);
+		}
+	}
+		
+	void PointOnMap()
+	{
+		Val<Ray> ray = Val.V (() => Camera.main.ScreenPointToRay (Input.mousePosition));
+		RaycastHit hit;
+		Physics.Raycast (ray.Value, out hit, 100);
+		Val.V (() => Physics.Raycast (ray.Value, out hit, 100));
+		positionToMove.position = (Val.V (() =>  hit.point)).Value;
+		UpdatePlayerPosition();
+	}
+
+	void UpdatePlayerPosition()
+	{
+		Val<Vector3> position = positionToMove.position;
+		participant.GetComponent<SteeringController> ().Target = position.Value;
+	}
+		
+}
+
